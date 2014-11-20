38e59c85b9261e685e9ccc97122b72df2db92cef
Gaurav Vichare <gauravvichare99@gmail.com>
2014-11-20 16:33:48 +0530

    Update connectivity.php

--- connectivity.php
+++ connectivity.php
@@ -1,11 +1,9 @@
 <?php
 
-	$db_name = "url";
-	$user_name = "root";
-	$pass = "";
+	$db_name = "gaurav99_url";
+	$user_name = "gaurav99_url";
+	$pass = "gaurav12345";
 	$server = "localhost";
-	$connection = mysql_connect($server, $user_name, $pass);
-	$db_select = mysql_select_db($db_name);
-	if(!$connection||!$db_name)
-   		die(mysql_error());
+	$connection = mysql_connect($server, $user_name, $pass)or die('Could not connect: ' . mysql_error());
+	$db_select = mysql_select_db($db_name)or die('Could not select database');
 ?>
